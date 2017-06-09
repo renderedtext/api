d6ec1576cf2f0fa7d2657e73b682f9ec2c69343f
Igor Šarčević <igor@renderedtext.com>
2017-06-09 17:09:50 +0200

    Request tables

--- templates/entities/entity_helper.rb
+++ templates/entities/entity_helper.rb
@@ -1,5 +1,31 @@
 require "json"
 
+def request_table(request)
+  headers = [ "Name", "Type", "Required", "Example" ]
+
+  data = request.fields.map do |f|
+    [ f.name, f.type, f.required?.inspect, f.example ]
+  end
+
+  title     = "| #{headers.join(" | ")} |"
+  seperator = "| #{headers.map { |h| '-' * h.size }.join(" | ")} |"
+  rows      = data.map { |row| "| #{row.join(" | ")} |" }.join("\n")
+
+"""
+### Params
+
+#{title}
+#{seperator}
+#{rows}
+"""
+end
+
+def routes
+  @resources.map do |resource|
+    resource.methods.map { |method| Route.new(resource, method) }
+  end.flatten
+end
+
 module JsonExample
   module_function
 
@@ -55,10 +81,54 @@ class Response
 
 end
 
+class Field
+  def initialize(raw)
+    @raw = raw
+  end
+
+  def name
+    @raw["key"]
+  end
+
+  def type
+    @raw["type"]
+  end
+
+  def required?
+    @raw["required"] == "true"
+  end
+
+  def example
+    if @raw["example"]
+      @raw["example"]
+    elsif @raw["examples"] && @raw["examples"].first
+      @raw["examples"].first["value"]
+    end
+  end
+end
+
+class Request
+  def initialize(raw)
+    @raw = raw
+  end
+
+  def empty?
+    @raw.nil?
+  end
+
+  def fields
+    @raw.first["properties"].map { |field| Field.new(field) }
+  end
+end
+
 class Route
   def initialize(resource, method)
     @resource = resource
     @method = method
+  end
+
+  def request
+    Request.new(@method.raw["body"])
   end
 
   def responses
@@ -81,86 +151,3 @@ class Route
     responses.find(&:succesfull?)
   end
 end
-
-def routes
-  @resources.map do |resource|
-    resource.methods.map { |method| Route.new(resource, method) }
-  end.flatten
-end
-
-# def body_entity_name(body)
-#   name = body["displayName"]
-
-#   name == body["key"] ? "-" : name
-# end
-
-# def resource_path(resource)
-#   "#{resource.raw["parentUrl"]}#{resource.raw["relativeUri"]}"
-# end
-
-# def table_line(fields)
-#   "| #{fields.join(" | ")} |"
-# end
-
-# def a_table(header, objects)
-#   return unless objects.to_a.any?
-
-#   header_line = table_line([header.keys])
-#   break_line  = table_line(header.count.times.map { "---" })
-
-#   body_lines = objects.map do |object|
-#     fields = header.values.map { |key| table_field(object, key) }
-
-#     table_line(fields)
-#   end
-
-#   lines = [header_line] + [break_line] + body_lines
-
-#   lines.join("\n")
-# end
-
-# def table_field(object, key)
-#   object = object.raw if object.respond_to?(:raw)
-
-#   value = object[key]
-
-#   if value.nil?
-#     "-"
-#   elsif value.kind_of?(Array)
-#     "[#{value.join(", ")}]"
-#   else
-#     value
-#   end
-# end
-
-# def uri_parameters_table(parameters)
-#   header = { "Name" => "displayName", "Type" => "type", "Required?" => "required" }
-
-#   a_table(header, parameters)
-# end
-
-# def properties_table(properties)
-#   headers_list = {
-#     "displayName" => "Name",
-#     "type" => "Type",
-#     "required" => "Required?",
-#     "format" => "Format",
-#     "enum" => "Enumeration"
-#   }
-
-#   keys = headers_list.keys & properties.map(&:raw).map(&:keys).flatten.uniq
-
-#   header = keys.map { |key| [headers_list[key], key] }.to_h
-
-#   a_table(header, properties)
-# end
-
-# def only_20x(responses)
-#   responses.select { |response| /^20\d$/.match(response.raw["code"]) }
-# end
-
-# def full_http_code(response)
-#   descriptions = { "200" => "OK", "204" => "No Content" }
-
-#   "#{response} #{descriptions[response]}"
-# end
