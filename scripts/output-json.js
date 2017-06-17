0f16759e9fc61f8456764e4884221f55cb9320df
Igor Šarčević <igor@renderedtext.com>
2017-06-17 12:22:17 +0200

    Remove everything that is unnecessary

--- scripts/output-json.js
+++
@@ -1,7 +0,0 @@
-var raml2obj = require('raml2obj');
-var fs = require('fs');
-
-raml2obj.parse("raml/api.raml").then((ramlObj) => {
-  var json_output = JSON.stringify(ramlObj);
-  fs.writeFile("json-output.json", json_output);
-});
