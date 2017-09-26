ddb04ac9c31bdb150e0947c8bf962e5beec52b43
Igor Šarčević <igor@renderedtext.com>
2017-09-26 16:54:20 +0200

    Rely on faraday for json parsing

--- semaphore-client-generator/client_source/lib/semaphore_client/http_client.rb
+++ semaphore-client-generator/client_source/lib/semaphore_client/http_client.rb
@@ -10,58 +10,38 @@ class SemaphoreClient
       @logger = logger
     end
 
-    def get(route_elements)
-      route = route(route_elements)
-
+    def get(path, params = nil)
       trace("GET", route) do
-        connection.get(route)
+        connection.get(route, params)
       end
     end
 
-    def post(route_elements, content = nil)
-      route = route(route_elements)
-
-      if content
-        trace("POST", route, content) do
-          connection.post(route, content)
-        end
-      else
-        trace("POST", route) do
-          connection.post(route)
-        end
+    def post(path, params = nil)
+      trace("POST", route, params) do
+        connection.post(route, params)
       end
     end
 
-    def patch(route_elements, content = nil)
-      route = route(route_elements)
-
-      if content
-        trace("PATCH", route, content) do
-          connection.patch(route, content)
-        end
-      else
-        trace("PATCH", route) do
-          connection.patch(route)
-        end
+    def patch(path, params = nil)
+      trace("PATCH", route, params) do
+        connection.patch(route, params)
       end
     end
 
-    def delete(route_elements)
-      route = route(route_elements)
-
-      trace("DELETE", route) do
-        connection.delete(route)
+    def delete(path, params = nil)
+      trace("DELETE", route, params) do
+        connection.delete(route, params)
       end
     end
 
     private
 
-    def trace(method, path, content = nil)
+    def trace(method, path, params = nil)
       if @verbose == true
         id = SecureRandom.hex
         started_at = Time.now.to_f
 
-        @logger.info "#{id} #{method} #{path} body: #{content.inspect}"
+        @logger.info "#{id} #{method} #{path} body: #{params.inspect}"
         response = yield
 
         finished_at = Time.now.to_f
@@ -73,15 +53,11 @@ class SemaphoreClient
       end
     end
 
-    def route(route_elements)
-      ["", @api_version, *route_elements].compact.join("/")
-    end
-
     def connection
-      @connection ||= Faraday.new(
-        :url => @api_url,
-        :headers => { "Authorization" => "Token #{@auth_token}" }
-      )
+      @connection ||= Faraday.new(:url => @api_url, :headers => { "Authorization" => "Token #{@auth_token}" }) do |conn|
+        conn.request :json
+        conn.response :json
+      end
     end
   end
 end
