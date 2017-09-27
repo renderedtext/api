8c192d11e98d5d1f5e6c0fb5c33eb9c8b47750cb
Igor Šarčević <igor@renderedtext.com>
2017-09-27 16:40:08 +0200

    Use Faraday's logger mechanism

--- semaphore-client-generator/client_source/lib/semaphore_client/http_client.rb
+++ semaphore-client-generator/client_source/lib/semaphore_client/http_client.rb
@@ -4,26 +4,24 @@ class SemaphoreClient
 
     class ResponseErrorMiddleware < Faraday::Middleware
       def call(env)
-        env = @app.call(env)
-
-        case env[:status]
-        when 401
-          raise SemaphoreClient::Exceptions::Unauthorized, env
-        when 404
-          raise SemaphoreClient::Exceptions::NotFound, env
-        when 405
-          raise SemaphoreClient::Exceptions::NotAllowed, env
-        when 409
-          raise SemaphoreClient::Exceptions::Conflict, env
-        when 422
-          raise SemaphoreClient::Exceptions::UnprocessableEntity, env
-        when 400...500
-          raise SemaphoreClient::Exceptions::BadRequest, env
-        when 500...600
-          raise SemaphoreClient::Exceptions::ServerError, env
+        @app.call(env).on_complete do |env|
+          case env[:status]
+          when 401
+            raise SemaphoreClient::Exceptions::Unauthorized, env
+          when 404
+            raise SemaphoreClient::Exceptions::NotFound, env
+          when 405
+            raise SemaphoreClient::Exceptions::NotAllowed, env
+          when 409
+            raise SemaphoreClient::Exceptions::Conflict, env
+          when 422
+            raise SemaphoreClient::Exceptions::UnprocessableEntity, env
+          when 400...500
+            raise SemaphoreClient::Exceptions::BadRequest, env
+          when 500...600
+            raise SemaphoreClient::Exceptions::ServerError, env
+          end
         end
-
-        env
       end
     end
 
@@ -36,55 +34,46 @@ class SemaphoreClient
     end
 
     def get(path, params = nil)
-      trace("GET", path) do
-        connection.get(path, params)
-      end
+      api_call(:get, path, params)
     end
 
     def post(path, params = nil)
-      trace("POST", path, params) do
-        connection.post(path, params)
-      end
+      api_call(:post, path, params)
     end
 
     def patch(path, params = nil)
-      trace("PATCH", path, params) do
-        connection.patch(path, params)
-      end
+      api_call(:patch, path, params)
     end
 
     def delete(path, params = nil)
-      trace("DELETE", path, params) do
-        connection.delete(path, params)
-      end
+      api_call(:delete, path, params)
     end
 
     private
 
-    def trace(method, path, params = nil)
-      if @verbose == true
-        id = SecureRandom.hex
-        started_at = Time.now.to_f
+    def api_call(method, path, params = nil)
+      route = "/#{@api_version}/#{path}"
 
-        @logger.info "#{id} #{method} #{path} body: #{params.inspect}"
-        response = yield
-
-        finished_at = Time.now.to_f
-        @logger.info "#{id} #{response.status} duration: #{finished_at - started_at}s body: #{response.body}"
-
-        response
-      else
-        yield
-      end
+      connection.public_send(method, route, params)
     end
 
     def connection
-      @connection ||= Faraday.new(:url => @api_url, :headers => { "Authorization" => "Token #{@auth_token}" }) do |conn|
-        conn.use SemaphoreClient::HttpClient::ResponseErrorMiddleware
-
+      @connection ||= Faraday.new(:url => @api_url, :headers => headers) do |conn|
         conn.request :json
         conn.response :json
+
+        if @verbose
+          conn.response :logger, @logger, :headers => false, :bodies => true
+        end
+
+        conn.use SemaphoreClient::HttpClient::ResponseErrorMiddleware
+
+        conn.adapter Faraday.default_adapter
       end
+    end
+
+    def headers
+      { "Authorization" => "Token #{@auth_token}" }
     end
   end
 end
